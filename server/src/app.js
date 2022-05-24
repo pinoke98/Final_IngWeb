edbb6d6ce900cc9d037a62b4a40ce3d18db5b5aa
pinoke98 <40706319+pinoke98@users.noreply.github.com>
2022-05-23 20:33:50 -0500

    Backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import config from './config'
 
-import Fact_VentasRoutes from './routes/Fact_Sales.routes'
+import PastelRoutes from './routes/Pastel.routes'
+import BarrasRoutes from './routes/Barras.routes'
+import BarrasDoblesRoutes from './routes/BarrasDobles.routes'
+import StackBarrasRoutes from './routes/StackBarras.routes'
+import MapRoutes from './routes/Map.routes'
 
 const app = express()
 
@@ -9,6 +13,10 @@ const app = express()
 //settings
 app.set('port', config.port)
 
-app.use(Fact_VentasRoutes)
+app.use(PastelRoutes)
+app.use(BarrasRoutes)
+app.use(BarrasDoblesRoutes)
+app.use(StackBarrasRoutes)
+app.use(MapRoutes)
 
 export default app
